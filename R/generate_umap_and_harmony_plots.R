85f1494208342f57c99db2dff4a6e7166724a32a
foocheung <foo.cheung@nih.gov>
2023-11-04 17:15:08 -0400

    Update generate_umap_and_harmony_plots.R
    u

--- R/generate_umap_and_harmony_plots.R
+++ R/generate_umap_and_harmony_plots.R
@@ -1,10 +1,10 @@
 generate_umap_and_harmony_plots <- function(B1_US_merge, save_name, lanes) {
   B1_US_merge@meta.data$Lane <- as.factor(B1_US_merge@meta.data$Lane)
   if (length(lanes) == 1){
- #   ggsave(paste0(save_name,"_umap.pdf"),
-    png(paste0(save_name,"_umap.png"), width=1800, height=1600, res = 300)
-    Seurat::DimPlot(object = B1_US_merge, reduction = "rna.umap", pt.size = .1, split.by = "BEST.GUESS")  + Seurat::NoLegend()
-    dev.off()
+    ggsave(paste0(save_name,"_umap.pdf"),
+ #   png(paste0(save_name,"_umap.png"), width=1800, height=1600, res = 300)
+  plot =  Seurat::DimPlot(object = B1_US_merge, reduction = "rna.umap", pt.size = .1, split.by = "BEST.GUESS"))  + Seurat::NoLegend()
+  #  dev.off()
   } else {
 
 
